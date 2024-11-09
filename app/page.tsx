bae59af7f21b286c92f1ac7fddde75bdcbd62358
Leandro Guedes <projectsleandro@gmail.com>
2024-11-09 16:17:00 -0300

    feat: add auth with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return (
-    <h1>HOME</h1>
-
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
   );
-}
+};
+
+export default Home;
